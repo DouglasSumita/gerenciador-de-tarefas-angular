92c0c31eab743636e6d37beafa3bbb31d4626862
Douglas Sumita <douglas.sumita@gmail.com>
2020-10-09 18:08:55 -0300

    Criado modulo de tarefas, e rota de navegaçao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TarefasModule } from './tarefas/tarefas.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    TarefasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
