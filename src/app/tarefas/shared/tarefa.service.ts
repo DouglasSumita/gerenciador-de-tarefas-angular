c54f7e2dac8a43c8baf5ab27bff49a046aaa0100
Douglas Sumita <douglas.sumita@gmail.com>
2020-10-13 08:10:51 -0300

    Adicionado servico de tarefas

--- src/app/tarefas/shared/tarefa.service.ts
+++ src/app/tarefas/shared/tarefa.service.ts
@@ -1,4 +1,12 @@
+/**
+ * Serviço de manipulação de tarefas.
+ *
+ * @author Douglas Sumita <douglas.sumita@gmail.com
+ * @since 1.0.0
+ */
+
 import { Injectable } from '@angular/core';
+import { Tarefa } from './tarefa.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +14,76 @@ import { Injectable } from '@angular/core';
 export class TarefaService {
 
   constructor() { }
+
+  /**
+   * Retorna uma lista de tarefas.
+   *
+   * @return Tarefa[]
+   */
+  listarTodos(): Tarefa[] {
+    const tarefas = localStorage['tarefas'];
+    return tarefas ? JSON.parse(tarefas) : [];
+  }
+
+  /**
+   * Cadastra uma nova tarefa no local storage.
+   *
+   * @param tarefa
+   * @return void
+   */
+  cadastrar(tarefa: Tarefa): void {
+    const tarefas = this.listarTodos();
+    tarefa.id = new Date().getTime();
+    tarefas.push(tarefa);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  /**
+   * Retorna a tarefa dado um Id.
+   *
+   * @param id number
+   * @return tarefa Tarefa
+   */
+  buscarPorId(id: number): Tarefa {
+    const tarefas: Tarefa[] = this.listarTodos();
+    return tarefas.find(tarefa => tarefa.id === id);
+  }
+
+  /**
+   * Atualiza uma tarefa na lista de tarefas.
+   *
+   * @param tarefa
+   * @return void
+   */
+  atualiza(tarefa: Tarefa): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if (tarefa.id === obj.id) {
+        objs[index] = tarefa;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  /**
+   * Remove a tarefa que tiver o id passado para o método.
+   *
+   * @param id
+   * @return void
+   */
+  remover(id: number): void {
+    let tarefas: Tarefa[] = this.listarTodos();
+    tarefas = tarefas.filter(tarefa => tarefa.id !== id);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  alterarStatus(id: number): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if (id === obj.id) {
+        objs[index].concluida = !obj.concluida;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
 }
